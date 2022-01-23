086cc096cac3d38211d2170eceeef25ae936cfc6
sanghun <sanghun1984@gmail.com>
2022-01-23 16:18:54 +0900

    init project

---
+++ src/main/java/kr/co/sanghun/study/jpaweb/JpaWebApplication.java
@@ -0,0 +1,13 @@
+package kr.co.sanghun.study.jpaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/sanghun/study/jpaweb/JpaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.sanghun.study.jpaweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
