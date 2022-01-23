f8eee9ac597317f63719c2be2ba8f48c6e93dcfd
sanghun <sanghun1984@gmail.com>
2022-01-23 17:37:56 +0900

    add jpa sample

--- src/main/java/kr/co/sanghun/study/jpaweb/MemberRepository.java
+++ src/main/java/kr/co/sanghun/study/jpaweb/MemberRepository.java
@@ -1,2 +1,22 @@
-package kr.co.sanghun.study.jpaweb;public class MemberRepository {
+package kr.co.sanghun.study.jpaweb;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
 }
--- src/test/java/kr/co/sanghun/study/jpaweb/MemberRepositoryTest.java
+++ src/test/java/kr/co/sanghun/study/jpaweb/MemberRepositoryTest.java
@@ -1,4 +1,34 @@
+package kr.co.sanghun.study.jpaweb;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
 import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
 class MemberRepositoryTest {
-  
+
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    @Transactional
+    @Rollback(false)
+    public void testMember()  throws Exception {
+        //given
+        Member member = new Member();
+        member.setUsername("memberA");
+
+        // when
+        Long saveId = memberRepository.save(member);
+        Member findMember = memberRepository.find(saveId);
+
+        // then
+        Assertions.assertEquals(findMember.getId(), member.getId());
+        Assertions.assertEquals(findMember.getUsername(), member.getUsername());
+        Assertions.assertEquals(findMember, member);
+    }
 }
